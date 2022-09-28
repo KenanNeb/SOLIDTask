254ed852ef4bdc814a258813eb2c56c3b1eb8fd9
NXbi_av4i@itstep.edu.az <NXbi_av4i@itstep.edu.az>
2022-09-28 22:56:44 +0400

    Add project files.

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace MyApp;
+
+//Bad example
+//public class Email
+//{
+//    public string ToAddress { get; set; }
+//    public string Subject { get; set; }
+//    public string Content { get; set; }
+//    public void SendEmail()
+//    {
+//        Console.WriteLine("Send email");
+//    }
+//}
+
+//public class SMS
+//{
+//    public string PhoneNumber { get; set; }
+//    public string Message { get; set; }
+//    public void SendSMS()
+//    {
+//        Console.WriteLine("Send SMS");
+
+//    }
+//}
+
+//public class Notification
+//{
+//    private Email _email;
+//    private SMS _sms;
+//    public Notification()
+//    {
+//        _email = new Email();
+//        _sms = new SMS();
+//    }
+
+//    public void Send()
+//    {
+//        _email.SendEmail();
+//        _sms.SendSMS();
+//    }
+//}
+// Good example
+public interface IMessage
+{
+    void SendMessage();
+}
+
+public class Email : IMessage
+{
+    public string ToAddress { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public void SendMessage()
+    {
+        Console.WriteLine("Send email");
+    }
+}
+
+public class SMS : IMessage
+{
+    public string PhoneNumber { get; set; }
+    public string Message { get; set; }
+    public void SendMessage()
+    {
+        Console.WriteLine("Send SMS");
+
+    }
+}
+public class Notification
+{
+    private ICollection<IMessage> _messages;
+    public Notification(ICollection<IMessage> messages)
+    {
+        this._messages = messages;
+    }
+    public void Send()
+    {
+        foreach (var message in _messages)
+        {
+            message.SendMessage();
+        }
+    }
+}
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ InterfaceSegregation/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace MyApp;
+// Bad example
+
+//interface ILaundry
+//{
+//    void WashClothes();
+//    void DryClothes();
+//}
+
+//class ComboWashingMashine : ILaundry
+//{
+//    public void WashClothes()
+//    {
+//        //do the work
+//    }
+
+//    public void DryClothes()
+//    {
+//        //do the work
+//    }
+//}
+
+//class SimpleWashingMashine : ILaundry
+//{
+//    public void WashClothes()
+//    {
+//        //yep, I can do that as well
+//    }
+
+//    public void DryClothes()
+//    {
+//        //oops, no can't do
+//        throw new NotImplementedException();
+//    }
+//}
+
+//Good Example
+interface IWashLaundry
+{
+    void WashClothes();
+}
+
+interface IDryLaundry
+{
+    void DryClothes();
+}
+
+class ComboWashingMashine : IWashLaundry, IDryLaundry
+{
+    public void WashClothes()
+    {
+        //do the work
+    }
+
+    public void DryClothes()
+    {
+        //do the work
+    }
+}
+
+class SimpleWashingMashine : IWashLaundry
+{
+    public void WashClothes()
+    {
+        //yep, I can do that as well
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
